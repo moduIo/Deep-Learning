62a6c038156e60c79b6bc1b8fdcf1d80480e6b61
Tim <ethosscream@yahoo.com>
2018-03-14 03:31:26 -0400

    Add baseline LSTM results

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 Implementations for Deep Neural Networks on a variety of tasks.
 
 ## Word2Vec
-Implements Word2Vec embedding layer using a sentiment analysis dataset and gensim.
-
-## CIFAR-10
-Implements a CNN for image classification on the CIFAR-10 dataset (https://www.cs.toronto.edu/~kriz/cifar.html).
-My model improves the baseline accuracy by 0.0539% to 0.8039% in 15 fewer epochs using Batch Normalization and Adam optimizer.
+Implements Word2Vec embedding layer using the IMDB sentiment analysis dataset and gensim.
 
 ## Sentiment Analysis
-Implements an LSTM for sentiment analysis on the IMDB dataset.
+Implements an LSTM in Keras for sentiment analysis on the IMDB sentiment analysis dataset.
+
+## CIFAR-10
+Implements a CNN in Keras for image classification on the CIFAR-10 dataset (https://www.cs.toronto.edu/~kriz/cifar.html).
+My model improves the baseline accuracy by 0.0539% to 0.8039% in 15 fewer epochs using Batch Normalization and Adam optimizer.
--- Sentiment Analysis/lstm.py
+++ Sentiment Analysis/lstm.py
@@ -1,43 +1,48 @@
 #
 # LSTM for sentiment analysis on the IMDB dataset.
-# Tutorial code: https://github.com/keras-team/keras/blob/master/examples/cifar10_cnn.py
-# Baseline Model: 75% validation accuracy in 25 epochs, and 79% after 50 epochs.
+# Tutorial code: https://github.com/keras-team/keras/blob/master/examples/imdb_lstm.py
+# Baseline Model: 0.8063% validation accuracy
 #
 from __future__ import print_function
-
 from keras.preprocessing import sequence
 from keras.models import Sequential
-from keras.layers import Dense, Embedding
-from keras.layers import LSTM
+from keras.layers import Dense, Embedding, LSTM
+from keras.layers.normalization import BatchNormalization
 from keras.datasets import imdb
 
+#
+# Global parameters
+#
 max_features = 20000
 maxlen = 80  # cut texts after this number of words (among top max_features most common words)
 batch_size = 32
 
+#
+# Load and process data
+#
 print('Loading data...')
 (x_train, y_train), (x_test, y_test) = imdb.load_data(num_words=max_features)
-print(len(x_train), 'train sequences')
-print(len(x_test), 'test sequences')
-
-print('Pad sequences (samples x time)')
 x_train = sequence.pad_sequences(x_train, maxlen=maxlen)
 x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
-print('x_train shape:', x_train.shape)
-print('x_test shape:', x_test.shape)
 
+#
+# LSTM Structure
+#
 print('Build model...')
 model = Sequential()
 model.add(Embedding(max_features, 128))
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(1, activation='sigmoid'))
+model.add(BatchNormalization())
+model.add(Dense(1, activation='tanh'))
 
-# try using different optimizers and different optimizer configs
 model.compile(loss='binary_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
-print('Train...')
+#
+# Training
+#
+print('Training...')
 model.fit(x_train, y_train,
           batch_size=batch_size,
           epochs=15,
